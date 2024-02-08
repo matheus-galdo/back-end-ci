aea888fc8cacb1391a6d034b8d2160eb8d3d7dfb
Matheus Galdino <matheus.kho@gmail.com>
2024-02-08 18:45:55 -0300

    mudanças

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ app.use("/", async (req: Request, res: Response) => {
   const messages = await prisma.message.findMany();
   if (messages.length === 0) return res.send("There are no messages!");
 
+  
   return res.send("um erro nos testes");
 });
 
